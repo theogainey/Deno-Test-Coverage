b006a58a6bb1d2dcebb0c4a0ce575b38b55dd203
Theo Gainey <theo@heysparkbox.com>
2022-04-06 12:15:43 -0400

    bug: fix bug

--- coverage.sh
+++ coverage.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 rm -fr /cov_profile/
 deno test --coverage=cov_profile
-deno coverage cov_profile --lcov > /cov_profile.lcov
-genhtml -o cov_profile/html /cov_profile.lcov
+deno coverage cov_profile --lcov > cov_profile/cov_profile.lcov
+genhtml -o cov_profile/html cov_profile/cov_profile.lcov
 open cov_profile/html/index.html
